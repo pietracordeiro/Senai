d883155460ada32d2ee6721ca2ea22f3274e6852
pietracordeiro <pietra_cordeiro@estudante.sesisenai.org.br>
2026-02-18 20:23:08 +0000

    update

---
+++ Atividade Python/Atividade01.py
@@ -0,0 +1,8 @@
+A = "Ola"
+B = "mundo"
+C = A+B
+print(C)
+d = 100
+e = 50
+f = d+e
+print (f)
---
+++ Atividade Python/atividade02.py
@@ -0,0 +1,7 @@
+A = input("insira um numero")
+B = input("insira o segumdo numero")
+print(A+B)
+
+A = float(input("insira um numero"))
+B = float(input("insira o segumdo numero"))
+print(A+B)
