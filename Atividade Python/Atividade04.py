65dc21b01874fb90eef43c48027b250b90c1e3f7
pietracordeiro <pietra_cordeiro@estudante.sesisenai.org.br>
2026-02-25 20:16:58 +0000

    update

---
+++ Atividade Python/Atividade04.py
@@ -0,0 +1,10 @@
+nota1 = float(input("insira a primeira nota"))
+nota2 = float(input("insira a segunda nota"))             
+nota3 = float(input("insira a terceira nota"))
+nota4 = float(input("insira a quarta nota"))
+media= (nota1+nota2+nota3+nota4)/4
+print("a media das notas: ",media)
+if media >= 7:
+    print("aluno está aprovado")
+else:
+    print ("aluno esta reprovado")
---
+++ Atividade Python/Atividade06.py
@@ -0,0 +1,9 @@
+nome = input("digite seu nome:")  
+idade = int(input("digite a sua idade: "))
+
+#repete quando a idade for valida
+while idade > 120 or idade < 0:
+    idade = int(input("idade(anos completos - ate 120 anos)"))
+dias_vida = idade * 365
+print(f"{nome}, voce viveu {dias_vida}")
+                                                                                                                                                                                                                                                                                            
